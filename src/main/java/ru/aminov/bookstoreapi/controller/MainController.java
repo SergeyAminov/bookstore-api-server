6538fa67e12025f94197e45e9ca17a1f4fb96085
Sergey Aminov <sergeyaminov@MacBook-Air-Sergey.local>
2024-02-23 22:07:12 +0300

    init

---
+++ src/main/java/ru/aminov/bookstoreapi/controller/MainController.java
@@ -0,0 +1,13 @@
+package ru.aminov.bookstoreapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    
+    @GetMapping(value = "/")
+    public String hello(){
+        return "Hello world";
+    }
+}
